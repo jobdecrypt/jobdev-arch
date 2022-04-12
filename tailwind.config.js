5fc3b25f6881ecc3728a22e811f1120039a7a5dd
jobdecrypt <76563339+jobdecrypt@users.noreply.github.com>
2022-04-12 17:57:01 -0400

    added @tailwindcss/typography /forms /line-clamp /aspect-ratio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 const colors = require('tailwindcss/colors');
-const defaultTheme = require('tailwindcss/defaulttheme');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/**/*.{js,ts,jsx,tsx}'],
